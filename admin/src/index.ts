a57002c3f4d057a11f02b0106ad662cfef4e9c5f
Prasundas99 <prasundas115@gmail.com>
2021-09-09 22:22:34 +0530

    connected to sql orm

--- admin/src/index.ts
+++ admin/src/index.ts
@@ -1,13 +1,15 @@
 import * as express from 'express';
 import * as cors from 'cors';
+import {createConnection} from 'typeorm'
 
-const app = express();
+createConnection().then(db => {
+    const app = express();
 
-app.use(cors());
-app.use(express.json())
-
-
-const PORT = process.env.PORT || 5000;
-app.listen(5000, () => {
-    console.log(`Server running on port ${PORT} `);
-  });
+    app.use(cors());
+    app.use(express.json())
+    
+    const PORT = process.env.PORT || 5000;
+    app.listen(5000, () => {
+        console.log(`Server running on port ${PORT} `);
+      });
+})

35e907dcbe776a6f79c80f4b1328618b3426d5c2
Prasundas99 <prasundas115@gmail.com>
2021-09-10 19:37:04 +0530

    Rabbit mq connected

--- admin/src/index.ts
+++ admin/src/index.ts
@@ -1,66 +1,87 @@
 import * as express from 'express';
 import { Request, Response } from 'express'
 import * as cors from 'cors';
+import * as dotenv from 'dotenv';
+
 import { createConnection } from 'typeorm'
 import { Product } from './entity/product';
+
+import * as amqp from 'amqplib/callback_api';
+
+dotenv.config();
 
 createConnection().then(db => {
     //db details
     const productRepository = db.getRepository(Product);
 
-    const app = express();
-    //middlewares
-    app.use(cors());
-    app.use(express.json())
+    //Rabbitmq connection
+    amqp.connect(process.env.RABBITMQ_URL, (error, connection) => {
+        if (error) {
+            throw error
+        }
+
+        connection.createChannel((error1, channel) => {
+            if(error1) {
+                throw error1
+            }
+            const app = express();
+            //middlewares
+            app.use(cors());
+            app.use(express.json())
 
 
-    //endpoints
+            //endpoints
 
-    //Get all products
-    app.get('/api/products', async (req: Request, res: Response) => {
-        const products = await productRepository.find();
-        res.json(products)
+            //Get all products
+            app.get('/api/products', async (req: Request, res: Response) => {
+                const products = await productRepository.find();
+                res.json(products)
+            })
+
+            //Create a single product to db
+            app.post('/api/products', async (req: Request, res: Response) => {
+                const product = await productRepository.create(req.body);
+                const result = await productRepository.save(product);
+                return res.send(result);
+            })
+
+            //Get single product from list of product
+            app.get('/api/products/:id', async (req: Request, res: Response) => {
+                const product = await productRepository.findOne(req.params.id);
+                return res.send(product)
+            })
+
+            //Update a single product from list of product
+            app.put('/api/products/:id', async (req: Request, res: Response) => {
+                const product = await productRepository.findOne(req.params.id);
+                productRepository.merge(product, req.body);
+                const result = await productRepository.save(product);
+                return res.send(result);
+            })
+
+            //Delete a single product
+            app.delete('/api/products/:id', async (req: Request, res: Response) => {
+                const result = await productRepository.delete(req.params.id);
+                return res.send(result);
+            })
+
+            //Request where we like the product
+            app.post('/api/products/:id/like', async (req: Request, res: Response) => {
+                const product = await productRepository.findOne(req.params.id);
+                product.likes++;
+                const result = await productRepository.save(product);
+                return res.send(result);
+            })
+
+
+            //server at PORT 5000
+            const PORT = process.env.PORT || 5002;
+            app.listen(PORT, () => {
+                console.log(`Server running on port ${PORT} `);
+            });
+        })
+
     })
-
-    //Create a single product to db
-    app.post('/api/products', async (req: Request, res: Response) => {
-        const product = await productRepository.create(req.body);
-        const result = await productRepository.save(product);
-        return res.send(result);
-    })
-
-    //Get single product from list of product
-    app.get('/api/products/:id', async (req: Request, res: Response) => {
-        const product = await productRepository.findOne(req.params.id);
-        return res.send(product)
-    })
-
-    //Update a single product from list of product
-    app.put('/api/products/:id', async (req: Request, res: Response) => {
-        const product = await productRepository.findOne(req.params.id);
-        productRepository.merge(product, req.body);
-        const result = await productRepository.save(product);
-        return res.send(result);
-    })
-
-    //Delete a single product
-    app.delete('/api/products/:id', async (req: Request, res: Response) => {
-        const result = await productRepository.delete(req.params.id);
-        return res.send(result);
-    })
-
-    //Request where we like the product
-    app.post('/api/products/:id/like', async (req: Request, res: Response) => {
-        const product = await productRepository.findOne(req.params.id);
-        product.likes++;
-        const result = await productRepository.save(product);
-        return res.send(result);
-    })
-
-
-    //server at PORT 5000
-    const PORT = process.env.PORT || 5000;
-    app.listen(5000, () => {
-        console.log(`Server running on port ${PORT} `);
-    });
 })
+
+
